7de6a73e825deb425de1addd889889f9db75d6ed
MylesSHarris <69069959+MylesSHarris@users.noreply.github.com>
2020-12-21 14:11:54 -0800

    Add files via upload

---
+++ Fraction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Lab
+{
+    public class Fraction
+    {
+        private double numerator, denominator;
+
+        public Fraction(double numerator, double denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public double Numerator
+        {
+            get { return numerator; }
+            set { numerator = value; }
+        }
+
+        public double Denominator
+        {
+            get { return denominator; }
+            set { denominator = value; }
+        }
+
+        private int FindGreatestCommonDenominator()
+        {
+            int greatest = 1;
+            for (int i = 1; i <= Math.Max(numerator, denominator); i++)
+            {
+                if (numerator % i == 0 && denominator % i == 0) greatest = i;
+            }
+            return greatest;
+        }
+
+        public void PrintReduced()
+        {
+            int greatestCommonDenominator = FindGreatestCommonDenominator();
+            Console.WriteLine($"{numerator / greatestCommonDenominator} / {denominator / greatestCommonDenominator}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Lab
+{
+    public class Program
+    {
+        public static void Main(String[] arguments)
+        {
+            Fraction fraction = new Fraction(5, 10);
+            fraction.PrintReduced();
+        }
+    }
+}
